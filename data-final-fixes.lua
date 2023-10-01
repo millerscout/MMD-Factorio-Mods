0c046dd9d0c60ddbd33dd07eb4b4ed2cdbc62a4b
Marcos Miller Dantas <mmillerdantas@outlook.com>
2023-10-01 00:57:49 -0300

    adds downgrade to eletric furnaces

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -25,8 +25,34 @@ if mods["RampantArsenal"] then
 end
 
 if mods["SteamEngineExtra"] then
-    data_util = require '__aai-industry__/data-util' 
+    data_util = require '__aai-industry__/data-util'
     if data.raw["fuel-category"]["processed-chemical"] then
         data_util.add_fuel_category(data.raw["boiler"]["adikings-boiler-mk2"].energy_source, "vehicle-fuel")
     end
+end
+if mods["Electric Furnaces"] then
+    data:extend(
+        {
+            {
+                type = "recipe",
+                name = "steel-furnace downgrade",
+                ingredients = {
+                    { "electric-steel-furnace", 1 },
+                    { "electronic-circuit",     2 },
+                    { "iron-plate",             2 }
+                },
+                result = "steel-furnace",
+                energy_required = 3,
+                enabled = true
+            },
+            {
+                type = "recipe",
+                name = "electric-stone-furnace downgrade",
+                ingredients = {
+                    { "electric-stone-furnace", 1 }
+                },
+                result = "stone-furnace",
+                enabled = true
+            }
+        })
 end
