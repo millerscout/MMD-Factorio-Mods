0724d6f4bb124e6ed13ae3e3e167a646f74b09dd
Marcos Miller Dantas <mmillerdantas@outlook.com>
2023-10-08 02:07:14 -0300

    0.0.14
       - balacing, reduce xp required to level up buildings, it takes alot of time in burning phase (4 hours)
       - better compatibility with space exploration, aai industry and krastorio2

--- xp-for-buildings/settings.lua
+++ xp-for-buildings/settings.lua
@@ -5,7 +5,7 @@ data:extend({
         minimum_value = 1,
         maximum_value = 5000,
         setting_type = "runtime-global",
-        default_value = 250
+        default_value = 180
 
     },
     {
@@ -31,7 +31,7 @@ data:extend({
         minimum_value = 0,
         maximum_value = 100,
         setting_type = "startup",
-        default_value = 0.05
+        default_value = 0.06
     },
     {
         type = "double-setting",
@@ -47,7 +47,7 @@ data:extend({
         minimum_value = 0,
         maximum_value = 100,
         setting_type = "startup",
-        default_value = 0.03
+        default_value = 0.04
     },
     {
         type = "double-setting",
@@ -55,7 +55,7 @@ data:extend({
         minimum_value = 0,
         maximum_value = 100,
         setting_type = "startup",
-        default_value = 0.01
+        default_value = 0.02
     },
     {
         type = "double-setting",
