7b8a86feab1e451f344481398945fc03aa9d73b3
Marcos Miller Dantas <mmillerdantas@outlook.com>
2023-10-15 14:20:00 -0300

    1.0.5
    changed stack sizes

--- MMD-QoL-Tweaks/data-final-fixes.lua
+++ MMD-QoL-Tweaks/data-final-fixes.lua
@@ -4,7 +4,9 @@ if mods["Krastorio2"] then
     if data.raw.recipe["kr-vc-kr-greenhouse"] then data.raw.recipe["kr-vc-kr-greenhouse"].hidden = true end
     if data.raw.recipe["craft-kr-greenhouse"] then data.raw.recipe["craft-kr-greenhouse"].hidden = true end
     if data.raw.technology["kr-greenhouse"] then data.raw.technology["kr-greenhouse"].hidden = true end
+    data.raw["item"]["sand"].stack_size = 1000
 end
+
 
 if mods["RampantIndustry"] then
     if data.raw.recipe["greenhouse-rampant-industry"] then data.raw.recipe["greenhouse-rampant-industry"].hidden = true end
@@ -28,6 +30,24 @@ if mods["SteamEngineExtra"] and mods["aai-industry"] then
     end
 end
 
-if  mods["Krastorio2"] then
-     data.raw["item"]["sand"].stack_size = 1000
+
+
+if mods["aai-industry"] then
+    data.raw["item"]["stone-tablet"].stack_size = 500
+    data.raw["item"]["electric-motor"].stack_size = 500
+    data.raw["item"]["engine-unit"].stack_size = 500
+    data.raw["item"]["motor"].stack_size = 500
+    data.raw["item"]["processed-fuel"].stack_size = 500
 end
+if mods["Bio_Industries"] then
+    data.raw["item"]["bi-woodpulp"].stack_size = 1000
+end
+
+if mods["SchallAlienLoot"] then
+    data.raw["item"]["alien-ore-1"].stack_size = 500
+    data.raw["item"]["alien-ore-2"].stack_size = 500
+    data.raw["item"]["alien-ore-3"].stack_size = 500
+    data.raw["item"]["small-alien-artifact"].stack_size = 500
+    
+end
+data.raw["item"]["iron-stick"].stack_size = 500
